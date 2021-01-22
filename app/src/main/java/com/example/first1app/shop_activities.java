48dea1ea5b4f5ab084e00a70a82c686ce2d026d8
Anish_Gulati <anishgulati890@gmail.com>
2021-01-22 23:08:10 +0530

    Update1

---
+++ app/src/main/java/com/example/first1app/shop_activities.java
@@ -0,0 +1,14 @@
+package com.example.first1app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class shop_activities extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop_activities);
+    }
+}
